dbf20330c0892b183b63e1f4e900c278b3a73d6f
Prathamesh1698 <117922146+Prathamesh1698@users.noreply.github.com>
2022-11-16 12:11:25 +0530

    Add files via upload

---
+++ Loop/loop_14nov(reverse).py
@@ -0,0 +1,7 @@
+n = int (input("enter a number : "))
+
+while n != 0:
+
+    r = n %10
+    print(r)
+    n = n // 10
---
+++ Loop/while.py
@@ -0,0 +1,29 @@
+'''
+i = 1
+n = int(input("enter a number: "))
+while i<=10:
+
+    r = n*i
+    print(r)
+
+    i +=1
+'''
+'''
+n = int(input("enter a number:"))
+i = 1
+s = 0
+while i <=n :
+    s = s + i
+    print( s , "+")
+    i += 1
+print("sum of first", n ,"natural numbers is", s )
+'''
+
+n = 5
+i = 1
+s = 1
+
+while i <= n:
+     s = s * i
+     i += 1
+print("factorial of ", n ,"is" , s)
